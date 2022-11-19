6283bf2ad80259ac4c82b2ba1de996e61b3b7acb
domssab <saballo.chantaldominique@gmail.com>
2022-11-19 22:39:32 +0800

    Introduction added

---
+++ Savings Calculator.py
@@ -0,0 +1,7 @@
+# Introduction
+print("GAME CURRENCY SAVINGS CALCULATOR")
+game_option = int(input("Which game would you want?"))
+      "\n1. Genshin Impact"
+      "\n2. Punishing: Gray Raven")
+
+if game_option == 1:
